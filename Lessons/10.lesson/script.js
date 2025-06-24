a30641f8864044a3bbf470af3b861fbfad15a7d8
Your Name <dace.liepina77@gmail.com>
2025-06-24 19:39:17 +0200

    init

---
+++ Lessons/10.lesson/script.js
@@ -0,0 +1,37 @@
+// ! arrays - массивы
+// сложный тип данных который хорошо подходит под перечисление чего-то с одинаковым набором свойств
+
+// jauna, tuksa masiiva izveide 
+const arr = [];
+
+const numbers = [4, 8, 15, 16, 23, 42];
+const fruits = ["apple", "orange", "pear"];
+
+// veersanaas pie elementa
+
+const pear = fruits[2];
+
+// если обратится к несуществующему индексу будет * undefine
+const none  = fruits[4]
+console.log(none);
+
+// mes varam izveidot masiivu no dazadiem elementa tipiem
+// но! как правило это не нужно и не удобно 
+
+const party = ["alien", 42, undefined,['🪐', '👨‍🚀']]
+
+console.log(party);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
