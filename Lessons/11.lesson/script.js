40d06635b907ce49fcab547ef9d8893538445863
Your Name <dace.liepina77@gmail.com>
2025-06-26 19:02:31 +0200

    init

---
+++ Lessons/11.lesson/script.js
@@ -0,0 +1,15 @@
+// ! объекты
+
+// пример объявления объекта
+const frodo = {
+    name: 'Frodo',
+    age: 30,
+    race: "hobbit",
+  hasMagic: false,
+  height: 60
+};
+
+// обращение к значению по ключу
+
+frodo.hasMagic = true;
+console.log(frodo.name)
