745bff4b281fe0bd0e0462ef4b58b3bbede2477b
Your Name <dace.liepina77@gmail.com>
2025-07-09 20:00:53 +0200

    init

---
+++ Homeworks/Promise_HW/script.js
@@ -0,0 +1,21 @@
+function myDay(){
+    return Math.random() < 0.5;
+}
+
+const dayPromise = new Promise(function(res, rej) {
+    setTimeout(() => {
+        if(myDay()){
+            res("Best day of my life");
+        }else{
+            rej(new Error("Something is off"));
+        }
+    }, 2000)
+});
+
+dayPromise
+  .then((day) => {
+    console.log(day);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
