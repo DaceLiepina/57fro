d2f284dc49075b58c1d2b611e82ab0e3e6c1cd72
Your Name <dace.liepina77@gmail.com>
2025-07-18 19:47:19 +0200

    init

--- Lessons/16.lesson/fake-store-app/js/signin.js
+++ Lessons/16.lesson/fake-store-app/js/signin.js
@@ -1,28 +1,34 @@
 const form = document.getElementById("login-form");
 const messageEl = document.getElementById("message");
 
-
 form.addEventListener("submit", (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-    const user = {
-        email: event.target.email.value,
-        password: event.target.password.value,
-    };
-    console.log(user);
-    fetchLogin(user);
+  const user = {
+    email: event.target.email.value,
+    password: event.target.password.value,
+  };
+  console.log(user);
+  fetchLogin(user);
 });
 
 async function fetchLogin(user) {
-    const res = await fetch("https://api.escuelajs.co/api/v1/auth/login", {
+  const res = await fetch("https://api.escuelajs.co/api/v1/auth/login", {
     method: "POST",
     body: JSON.stringify(user),
     headers: { "Content-Type": "application/json" },
   });
 
-  if (res.ok) {
-    messageEl.textContent = "Login successful!";
+
+ if (!res.ok) {
+    const err = await res.json();
+    const { message } = err;
+    messageEl.textContent = message;
+    messageEl.style.color = "red";
   } else {
-    messageEl.textContent = "Login failed!";
+    const tokenObj = await res.json();
+    console.log(tokenObj);
+    localStorage.setItem("accessToken", access_token);
+    window.location.href = "/profile"
   }
 }
